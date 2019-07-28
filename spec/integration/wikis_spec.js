7ef09b5cf90fcb9d3fc9df9aa31d02ce1308cd37
KReysen <43965763+KReysen@users.noreply.github.com>
2019-07-28 13:57:28 -0600

    passing integration spec

--- spec/integration/wikis_spec.js
+++ spec/integration/wikis_spec.js
@@ -1,14 +1,41 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/wikis/";
+const sequelize = require("../../src/db/models/index").sequelize;
+const Wiki = require("../../src/db/models").Wiki;
+
+
 
 describe("routes : wikis", () => {
+    beforeEach((done) => {
+        this.wiki;
+        sequelize.sync({force: true}).then((res) => {
+            Wiki.create({
+                title: "JS Frameworks",
+                body: "There are a lot of them",
+                private: false 
+            })
+            .then((wiki) => {
+                this.wiki = wiki;
+                done();
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            });
+        });
+    });
+
     describe("GET /wikis", () => {
-        it("should return a status code 200", (done) => {
+        it("should return a status code 200 and all wikis", (done) => {
             request.get(base, (err, res, body) => {
               expect(res.statusCode).toBe(200);
+              expect(err).toBeNull();
+              expect(body).toContain("Wikis");
+              expect(body).toContain("JS Frameworks");
               done();
             });
         });
     });
+
 });
--- src/db/models/wiki.js
+++ src/db/models/wiki.js
@@ -3,10 +3,24 @@ module.exports = (sequelize, DataTypes) => {
   var Wiki = sequelize.define('Wiki', {
     title: DataTypes.STRING,
     body: DataTypes.STRING,
-    private: DataTypes.BOOLEAN
+    private: DataTypes.BOOLEAN,
+    userId: {
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Users",
+        key: "id",
+        as: "userId",
+      }
+    }
   }, {});
+
   Wiki.associate = function(models) {
     // associations can be defined here
+    Wiki.belongsTo(models.User, {
+      foreignKey: "userId",
+      onDelete: "CASCADE",
+    });
   };
   return Wiki;
 };
